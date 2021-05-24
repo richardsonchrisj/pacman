b0af80b760212f8affbd45fa6190ddb4a4aeb4df
Chris <richardsonchrisj@gmail.com>
2021-05-24 18:26:27 -0400

    made page width dynamic

--- pacman.js
+++ pacman.js
@@ -1,22 +1,24 @@
-// pos is the PacMan image position variable- it is set to 0 initially
+//Position of PAC-MAN
 var pos = 0;
-//pageWidth is the width of the webpage. This is later used to calculate when Pac-Man needs to turn around. 
+
+//Width of the webpage. This is later used to calculate when PAC-MAN needs to turn around. 
 let pageWidth = window.innerWidth;
-//This array contains all the PacMan movement images
+
+//This array contains all the PAC-MAN movement images
 const pacArray = [
   ['./images/PacMan1.png', './images/PacMan2.png'],
   ['./images/PacMan3.png', './images/PacMan4.png'],
 ];
 
-// this variable defines what direction should PacMan go into:
+// PAC-MAN's direction:
 // 0 = left to right
 // 1 = right to left (reverse)
 var direction = 0;
 
-// This variable helps determine which PacMan image should be displayed. It flips between values 0 and 1
+// Determines which PAC-MAN image should be displayed. It flips between values 0 and 1
 var focus = 0;
 
-// This function is called on mouse click. Every time it is called, it updates the PacMan image, position and direction on the screen.
+// This function is called on mouse click. Every time it is called, it updates the PAC-MAN image, position and direction on the screen.
 function Run() {
   let img = document.getElementById('PacMan');
   let imgWidth = img.width;
@@ -30,17 +32,15 @@ function Run() {
     pos += 20;
     img.style.left = pos + 'px';
   }
-  console.log("pos: " + pos);
 }
-// TODO: Add a Javascript setInterval() method that will call the Run() function above every 200 milliseconds. Note: in the video, Dr. Williams uses the setTimeout() method, but here we are going to use a slightly different
-// method called setInterval(), so that you can have practice using this method.
-// Inside of the Run() function you will also have to add an extra argument "pageWidth", which is declared on line 4 when you call the checkPageBounds() function below. 
+// setInterval() method calls the Run() function above every 200 milliseconds. 
 setInterval(Run, 200);
 
-// This function determines the direction of PacMan based on screen edge detection. 
+// Determines the direction of PAC-MAN based on screen edge detection. 
 function checkPageBounds(direction, imgWidth, pos, pageWidth) {
-  //
-  // TODO: Complete this to reverse direction upon hitting screen edge
+
+  pageWidth = window.innerWidth;
+  //Reverse direction upon hitting screen edge
   if (direction) {
     if (pos < 0) {
       direction = 0;
@@ -52,9 +52,5 @@ function checkPageBounds(direction, imgWidth, pos, pageWidth) {
       direction = 1;
     }
   }
-  //
   return direction;
 }
-
-//Please do not change
-// module.exports = checkPageBounds;
